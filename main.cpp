9401407417db1a9b87a0bc54a9df9311d33b258e
Bob(ErmakovaPA) <bob@example.com]>
2022-03-07 00:51:30 +0300

    code: добавлен вывод минимума

--- main.cpp
+++ main.cpp
@@ -8,10 +8,13 @@ int main(){
     cin >> a >> b;
     int max = a;
     if (b > a) max = b;
+    int min = a;
+    if (b < a) min = b;
     cout << "a + b = "<< a + b << '\n'
     << "a - b = "<< a - b << '\n'
     << "a * b = "<< a * b << '\n'
     << "a / b = "<< a / b << '\n'
-    <<"max in numbers:" << max <<'\n';
+    <<"max in numbers:" << max <<'\n'
+    << "min in numbers:" << min << '\n';
     return 0;
 }
