5e920be18445d1ba19816a65d4c7a248ca799c69
Alice (ErmakovaPA) <alice@example.com>
2022-03-06 21:15:00 +0300

    code : добавлен вывод разности a и b

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(){
     cout << "Enter A and B: ";
     int a, b;
     cin >> a >> b;
-    cout << "a + b = "<< a + b;
+    cout << "a + b = "<< a + b << '\n';
+    cout << "a - b = "<< a - b << '\n';
     return 0;
 }
